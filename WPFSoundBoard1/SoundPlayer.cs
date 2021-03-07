59a9841037dfb93008fd092140f8fad96c72bf87
GIDDE1\zleep <christian_elm@hotmail.com>
2021-03-07 18:01:50 +0100

    added SoundPlayer class

---
+++ WPFSoundBoard1/SoundPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+
+namespace WPFSoundBoard1
+{
+    class SoundPlayer
+    {
+        public void PlaySound()
+        {
+            
+        }
+
+        public void StopSound()
+        {
+
+        }
+
+    }
+
+}
